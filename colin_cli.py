ae4ca1cd45a306444d7019b63b81938484685ce1
Tom Fellowes <thomas@fellonet.com>
2020-02-13 12:55:52 +1100

    fixes bugs in cli

--- colin_cli.py
+++ colin_cli.py
@@ -6,7 +6,7 @@ from prettytable import PrettyTable
 from pint import UnitRegistry
 from PIL import Image,ImageDraw,ImageFont
 
-host = 'colin-uom.herokuapp.com'
+host = 'colin-uom-dev.herokuapp.com'
 port = '80'
 
 hostport = host + ':' + port
@@ -80,9 +80,10 @@ def createChemical():
     prefix = chemical[0].get('prefix','')
     name = chemical[0]['name']
     haz_substance = chemical[0]['haz_substance']
-    dg_class = chemical[0].get('dg_class',{}).get('number','')
-    dg_class_2 = chemical[0].get('dg_class_2',{}).get('number','')
-    dg_class_3 = chemical[0].get('dg_class_3',{}).get('number','')
+    dg_class = {}
+    dg_class[0] = chemical[0].get('dg_class',{}).get('number','')
+    dg_class[1] = chemical[0].get('dg_class_2',{}).get('number','')
+    dg_class[2] = chemical[0].get('dg_class_3',{}).get('number','')
     packing_group = chemical[0].get('packing_group',{}).get('name','')
     un_number = chemical[0].get('un_number','')
     schedule = chemical[0].get('schedule',{}).get('number','')
@@ -107,7 +108,7 @@ def createChemical():
   size_unit = str(ureg(container_size_string).units)
 
   if click.confirm('Create this chemical?'):
-    url = "http://" + hostport + "/api/container/serial/" + str(serial_number) +"?cas=" + str(cas) + "&prefix=" + str(prefix) + "&name=" + str(name) + "&dg_class=" + str(dg_class[0]) + "&dg_class_2=" + str(dg_class[1]) + "dg_class_3=" + str(dg_class[2]) + "&schedule=" + str(schedule) + "packing_group=" + str(packing_group) + "&un_number=" + str(un_number) + "&haz_substance=" + str(haz_substance) + "&container_size=" + str(container_size) + "&size_unit=" + str(size_unit) + "&supplier=" + str(supplier) + "&location=" + str(location) + "&supplier=" + str(supplier) + "&description=" + str(description)
+    url = "http://" + hostport + "/api/container/serial/" + str(serial_number) +"?cas=" + str(cas) + "&prefix=" + str(prefix) + "&name=" + str(name) + "&dg_class=" + str(dg_class[0]) + "&dg_class_2=" + str(dg_class[1]) + "dg_class_3=" + str(dg_class[2]) + "&schedule=" + str(schedule) + "&packing_group=" + str(packing_group) + "&un_number=" + str(un_number) + "&haz_substance=" + str(haz_substance) + "&container_size=" + str(container_size) + "&size_unit=" + str(size_unit) + "&supplier=" + str(supplier) + "&location=" + str(location) + "&supplier=" + str(supplier) + "&description=" + str(description)
     try:
       requests.post(url)
     except:
